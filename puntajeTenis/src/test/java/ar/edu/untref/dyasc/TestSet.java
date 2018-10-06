2c1dbebd6e88233150267486d4c0051a9e47c8d2
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2018-10-05 21:39:26 -0300

    Fin de la clase

---
+++ puntajeTenis/src/test/java/ar/edu/untref/dyasc/TestSet.java
@@ -0,0 +1,23 @@
+
+package ar.edu.untref.dyasc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Enums.Jugadores;
+import Enums.Puntaje;
+
+public class TestSet {
+    /*@Test
+    public void creacionSetSinPuntaje(){
+        Set set = new Set();
+        
+        
+        
+        Puntaje puntajeJ1 = game.getPuntaje(Jugadores.JUGADOR_1);
+        Puntaje puntajeJ2 = game.getPuntaje(Jugadores.JUGADOR_2);
+        
+        Assert.assertEquals(Puntaje._0, puntajeJ1);
+        Assert.assertEquals(Puntaje._0, puntajeJ2);
+    }*/
+}
