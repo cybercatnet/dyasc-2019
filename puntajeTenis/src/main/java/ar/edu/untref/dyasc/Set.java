086e441728edfb142a154ea506cf7ac8461ce91d
Alan N. Achaval <achavalalann@gmail.com>
2018-10-14 15:46:41 -0300

    Sumar un punto dentro del Set

--- puntajeTenis/src/main/java/ar/edu/untref/dyasc/Set.java
+++ puntajeTenis/src/main/java/ar/edu/untref/dyasc/Set.java
@@ -1,5 +1,14 @@
 package ar.edu.untref.dyasc;
 
+import Enums.Jugadores;
+
 public class Set {
+
+    public int getPuntaje(Jugadores jugador2) {
+        return 0;
+    }
+
+    public void agregarPunto(Jugadores jugador1) {
+    }
 
 }
--- puntajeTenis/src/test/java/ar/edu/untref/dyasc/TestSet.java
+++ puntajeTenis/src/test/java/ar/edu/untref/dyasc/TestSet.java
@@ -5,19 +5,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import Enums.Jugadores;
-import Enums.Puntaje;
 
 public class TestSet {
-    /*@Test
-    public void creacionSetSinPuntaje(){
+
+    @Test
+    public void crearUnSetYVerificarPuntajeInicialJugadores() {
         Set set = new Set();
-        
-        
-        
-        Puntaje puntajeJ1 = game.getPuntaje(Jugadores.JUGADOR_1);
-        Puntaje puntajeJ2 = game.getPuntaje(Jugadores.JUGADOR_2);
-        
-        Assert.assertEquals(Puntaje._0, puntajeJ1);
-        Assert.assertEquals(Puntaje._0, puntajeJ2);
-    }*/
+
+        int puntajeJ1 = set.getPuntaje(Jugadores.JUGADOR_1);
+        int puntajeJ2 = set.getPuntaje(Jugadores.JUGADOR_2);
+
+        Assert.assertEquals(0, puntajeJ1);
+        Assert.assertEquals(0, puntajeJ2);
+    }
+
+    @Test
+    public void sumar1PuntosAlJugador1() {
+        Set set = new Set();
+
+        set.agregarPunto(Jugadores.JUGADOR_1);
+        int puntajeJ1 = set.getPuntaje(Jugadores.JUGADOR_1);
+        int puntajeJ2 = set.getPuntaje(Jugadores.JUGADOR_2);
+
+        Assert.assertEquals(0, puntajeJ1);
+        Assert.assertEquals(0, puntajeJ2);
+    }
 }
